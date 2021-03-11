68d47473699aaf8444db1aff69743ee41aa93748
Noble <noblebeazel@gmail.com>
2021-03-11 08:32:35 -0700

    ModelData

---
+++ Workout-Capstone/Workout-Capstone/Models/ExerciseData.swift
@@ -0,0 +1,33 @@
+//
+//  ExerciseData.swift
+//  Workout-Capstone
+//
+//  Created by Noble Beazel on 3/11/21.
+//
+
+import Foundation
+import UIKit
+
+
+
+enum MuscleGroup {
+    case legs
+    case shoulders
+    case core
+    case biceps
+    case back
+    case cardio
+    
+}
+
+struct Exercise {
+    var name: String
+    var image: UIImage
+    var timer: Timer
+    var reps: Int
+    var requiresEquipment: Bool
+    var muscle: MuscleGroup?
+    
+}
+
+
