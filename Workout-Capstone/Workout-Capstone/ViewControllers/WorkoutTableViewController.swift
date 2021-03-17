a6649097d9250b4823cf733b618247f251ba6e5c
Noble <noblebeazel@gmail.com>
2021-03-17 08:22:23 -0600

    Updated Workout TableView
    swipe to delete
    diffable datasource
    adjusted layout

--- Workout-Capstone/Workout-Capstone/ViewControllers/WorkoutTableViewController.swift
+++ Workout-Capstone/Workout-Capstone/ViewControllers/WorkoutTableViewController.swift
@@ -10,91 +10,86 @@ import UIKit
 
 
 class WorkoutTableViewController: UITableViewController {
-
+    
+    var exercises = [Exercise]()
+    var dataSource: ExerciseDataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
         
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "exerciseCell", for: indexPath) as! WorkoutExercisesTableViewCell
-
-        // Configure the cell...
-
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 50
-    }
-    
-    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.width, height: 50))
-        footerView.backgroundColor = .darkGray
-        return footerView
+        for num in 1...10 {
+            var exercise = Exercise()
+            exercise.image = UIImage(systemName: "person.fill")
+            exercise.name = "Exercise \(num)"
+            exercise.reps = 420
+            exercises.append(exercise)
+        }
+                
+        configureDataSource()
     }
     
     
+    func configureDataSource() {
+        dataSource = ExerciseDataSource(tableView: tableView, cellProvider: { (tableView, indexPath, exercise) -> UITableViewCell? in
+            tableView.allowsSelection = false
+            let cell = tableView.dequeueReusableCell(withIdentifier: "exerciseCell", for: indexPath) as! WorkoutExercisesTableViewCell
+            
+            cell.exerciseNameLabel.text = exercise.name
+            cell.exerciseTimeLabel.text = "\(exercise.timer)"
+            cell.exerciseImageView.image = exercise.image
+            cell.repCountLabel.text = "\(exercise.reps)"
+            
+            return cell
+            
+        })
+        updateDataSource()
+    }
     
+    func deleteExercise(exercise: Exercise) {
+        var snapshot = dataSource.snapshot()
+        snapshot.deleteItems([exercise])
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
     
-
-    /*
-    // Override to support conditional editing of the table view.
+    func updateDataSource() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Exercise>()
+        let section = Section.main
+        snapshot.appendSections([section])
+        snapshot.appendItems(exercises, toSection: section)
+        
+        dataSource.apply(snapshot, animatingDifferences: true)
+        
+    }
+    
+    //MARK: - TableView Func
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (_, _, completion) in
+            guard let exercise = self.dataSource.itemIdentifier(for: indexPath) else {
+                return
+            }
+            self.deleteExercise(exercise: exercise)
+            
+        }
+        let actions = UISwipeActionsConfiguration(actions: [deleteAction])
+        return actions
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+class ExerciseDataSource: UITableViewDiffableDataSource<WorkoutTableViewController.Section, Exercise> {
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
+
+extension WorkoutTableViewController {
+     enum Section {
+        case main
+    }
+}
+
