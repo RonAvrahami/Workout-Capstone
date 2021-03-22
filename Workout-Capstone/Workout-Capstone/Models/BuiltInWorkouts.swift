c5feea8461d81c9e44264fddccb02326e0fd7320
Noble <noblebeazel@gmail.com>
2021-03-22 08:12:02 -0600

    changedToClass

--- Workout-Capstone/Workout-Capstone/Models/BuiltInWorkouts.swift
+++ Workout-Capstone/Workout-Capstone/Models/BuiltInWorkouts.swift
@@ -6,7 +6,11 @@
 //
 
 import Foundation
+class BuiltInWorkouts {
+    
+
 var workout1 = Workout(name: "Workout1", image: nil, timer: nil, exercises: [inclineDumbbellPress, latPullDown, skullCrushers, frontRaise, boxJump], requiresEquipment: true)
 var workout2 = Workout(name: "Workout2", image: nil, timer: nil, exercises: [barbellBenchPress, wideGripPullUps, barbellBicepCurl, dumbbellSquatWalk, dumbbellJumpSquat], requiresEquipment: true)
 var wokrout3 = Workout(name: "Workout3", image: nil, timer: nil, exercises: [inclineBenchPress, singleArmDumbbellRow, cableTricepPushDown, bentOverDumbbellLatRaise, deadlift], requiresEquipment: true)
-
+var workout4 = Workout(name: "Chest Day", image: nil, timer: nil, exercises: [barbellBenchPress, skullCrushers, inclineBenchPress, jumpRope, pullUps, dips, latPullDown, boxJump], requiresEquipment: true)
+}
